9250a863354d4e54ee1f34e3972ea643f5a46e60
rvargass <62122622+rvargass@users.noreply.github.com>
2022-03-30 16:26:54 -0300

    alteracoes

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,14 +13,27 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end' }
-  ];
+  courses$: Observable<Course[]>;
+  // courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor(private coursesService: CoursesService,
+    public dialog: MatDialog) {
 
-  ngOnInit(): void {
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos!');
+          return of([])
+        })
+      );
   }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void { }
 }
